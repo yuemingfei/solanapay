9700cca162a6783e129eafdcc43537fbc9d03a21
yuemingfei <guxiagufei@gmail.com>
2022-04-12 20:03:52 +0800

    pay

---
+++ lib/addresses.ts
@@ -0,0 +1,6 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('3vUXaV8hjYddGpMACiisTKcKws1TujdEL5ovEmxERi4L')
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+export const couponAddress = new PublicKey('Bmrj6hgQRYd7jCgENXC2tMCH5rRAtiHmrnJpFBTPpFxn')
